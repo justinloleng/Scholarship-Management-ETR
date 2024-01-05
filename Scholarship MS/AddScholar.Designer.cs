8edc7a3877e6bdd9e3d75ae790b05e2a52e1d43a
justinloleng <jtrooper10@gmail.com>
2024-01-05 16:18:57 +0800

    first commit

---
+++ Scholarship MS/AddScholar.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Scholarship_MS
+{
+    partial class AddScholar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.funds = new System.Windows.Forms.Button();
+            this.scholars_list = new System.Windows.Forms.Button();
+            this.add_scholars = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(71)))));
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(173, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(627, 59);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Scholarship_MS.Properties.Resources.image_44;
+            this.pictureBox1.Location = new System.Drawing.Point(51, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(68, 68);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(50)))), ((int)(((byte)(137)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.funds);
+            this.panel1.Controls.Add(this.scholars_list);
+            this.panel1.Controls.Add(this.add_scholars);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(173, 450);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // funds
+            // 
+            this.funds.FlatAppearance.BorderSize = 0;
+            this.funds.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.funds.Font = new System.Drawing.Font("Segoe UI Historic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.funds.ForeColor = System.Drawing.Color.White;
+            this.funds.Location = new System.Drawing.Point(0, 283);
+            this.funds.Name = "funds";
+            this.funds.Size = new System.Drawing.Size(173, 33);
+            this.funds.TabIndex = 3;
+            this.funds.Text = "Funds";
+            this.funds.UseVisualStyleBackColor = true;
+            // 
+            // scholars_list
+            // 
+            this.scholars_list.FlatAppearance.BorderSize = 0;
+            this.scholars_list.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.scholars_list.Font = new System.Drawing.Font("Segoe UI Historic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scholars_list.ForeColor = System.Drawing.Color.White;
+            this.scholars_list.Location = new System.Drawing.Point(0, 235);
+            this.scholars_list.Name = "scholars_list";
+            this.scholars_list.Size = new System.Drawing.Size(173, 33);
+            this.scholars_list.TabIndex = 2;
+            this.scholars_list.Text = "Scholars List";
+            this.scholars_list.UseVisualStyleBackColor = true;
+            // 
+            // add_scholars
+            // 
+            this.add_scholars.FlatAppearance.BorderSize = 0;
+            this.add_scholars.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.add_scholars.Font = new System.Drawing.Font("Segoe UI Historic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add_scholars.ForeColor = System.Drawing.Color.White;
+            this.add_scholars.Location = new System.Drawing.Point(0, 187);
+            this.add_scholars.Name = "add_scholars";
+            this.add_scholars.Size = new System.Drawing.Size(173, 33);
+            this.add_scholars.TabIndex = 1;
+            this.add_scholars.Text = "Add Scholars";
+            this.add_scholars.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Historic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(40, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Scholarship";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Historic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(8, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Management System";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Historic", 12F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(20, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Welcome Admin";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(71)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(173, 412);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(627, 38);
+            this.panel3.TabIndex = 2;
+            // 
+            // AddScholar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "AddScholar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Scholar";
+            this.Load += new System.EventHandler(this.AddScholar_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button funds;
+        private System.Windows.Forms.Button scholars_list;
+        private System.Windows.Forms.Button add_scholars;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
